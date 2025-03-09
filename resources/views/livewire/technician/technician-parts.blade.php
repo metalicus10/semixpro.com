d37cfe4e79bbaab8846cf02f671451971fecf6dd
Vladimir Scherbina <scherbinavl89@gmail.com>
2025-03-10 02:05:13 +1000

    Technician parts editing-

--- resources/views/livewire/technician/technician-parts.blade.php
+++ resources/views/livewire/technician/technician-parts.blade.php
@@ -31,9 +31,6 @@
     </div>
 
 
-
-
-
     <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
         <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab" data-tabs-toggle="#default-tab-content" role="tablist">
             <li class="me-2" role="presentation">
